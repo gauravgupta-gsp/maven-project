beb0d9819d75dde52e21ed925d07e7c4d9010b4b
GauravGupta <gauravgupta.gsp@gmail.com>
2018-02-27 11:38:31 +0530

    removed trailing spaces from Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  * @param someone who will be greeted  
-  * @return string 
+  * @param someone who will be greeted
+  * @return string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
